ea1d0cdfaae487987493cd3bf7fba9af4fb3151c
Peter Major <peter@petermajor.co.uk>
2016-01-13 21:31:38 +0000

    Use constructor injection rather than service locator

--- Sparkol.Android/Commands/OpenLinkCommand.cs
+++ Sparkol.Android/Commands/OpenLinkCommand.cs
@@ -2,16 +2,22 @@
 using Android.Content;
 using Android.Net;
 using Android.App;
-using Cirrious.CrossCore;
 using Cirrious.CrossCore.Droid.Platform;
 
 namespace Sparkol.Android.Commands
 {
 	public class OpenLinkCommand : IOpenLinkCommand
 	{
+		readonly IMvxAndroidCurrentTopActivity _topActivityProvider;
+
+		public OpenLinkCommand(IMvxAndroidCurrentTopActivity topActivityProvider)
+		{
+			_topActivityProvider = topActivityProvider;
+		}
+
 		public void Execute (string link)
 		{
-			var activity = Mvx.Resolve<IMvxAndroidCurrentTopActivity>().Activity;
+			var activity = _topActivityProvider.Activity;
 
 			var uri = Uri.Parse (link);
 			var intent = new Intent(Intent.ActionView, uri);
